1274a387183f1958a720adfe399254f63afd1789
unknown <dkim.damian@gmail.com>
2023-02-28 07:30:10 -0800

    converted problematic proptype to any

--- src/Notification.js
+++ src/Notification.js
@@ -1,169 +1,183 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import { Animated, StyleSheet, Image } from 'react-native';
-import { getStatusBarHeight, isIphoneX } from 'react-native-iphone-x-helper';
+import React, { Component } from "react";
+import PropTypes, { any } from "prop-types";
+import { Animated, StyleSheet, Image } from "react-native";
+import { getStatusBarHeight, isIphoneX } from "react-native-iphone-x-helper";
 
-import DefaultNotificationBody from './DefaultNotificationBody';
+import DefaultNotificationBody from "./DefaultNotificationBody";
 
 const styles = StyleSheet.create({
-  notification: {
-    position: 'absolute',
-    width: '100%',
-  },
+    notification: {
+        position: "absolute",
+        width: "100%",
+    },
 });
 
 class Notification extends Component {
-  constructor() {
-    super();
+    constructor() {
+        super();
 
-    this.heightOffset = isIphoneX() ? getStatusBarHeight() : 0;
+        this.heightOffset = isIphoneX() ? getStatusBarHeight() : 0;
 
-    this.show = this.show.bind(this);
-    this.showNotification = this.showNotification.bind(this);
-    this.closeNotification = this.closeNotification.bind(this);
+        this.show = this.show.bind(this);
+        this.showNotification = this.showNotification.bind(this);
+        this.closeNotification = this.closeNotification.bind(this);
 
-    this.state = {
-      animatedValue: new Animated.Value(0),
-      isOpen: false,
-    };
-  }
-
-  show(
-    { title, message, onPress, icon, vibrate, additionalProps } = {
-      title: '',
-      message: '',
-      onPress: null,
-      icon: null,
-      vibrate: true,
-      additionalProps: {},
-    },
-  ) {
-    const { closeInterval } = this.props;
-    const { isOpen } = this.state;
-
-    // Clear any currently showing notification timeouts so the new one doesn't get prematurely
-    // closed
-    clearTimeout(this.currentNotificationInterval);
-
-    const showNotificationWithStateChanges = () => {
-      this.setState(
-        {
-          isOpen: true,
-          title,
-          message,
-          onPress,
-          icon,
-          vibrate,
-          additionalProps,
-        },
-        () => this.showNotification(() => {
-          this.currentNotificationInterval = setTimeout(() => {
-            this.setState(
-              {
-                isOpen: false,
-                title: '',
-                message: '',
-                onPress: null,
-                icon: null,
-                vibrate: true,
-                additionalProps,
-              },
-              this.closeNotification,
-            );
-          }, closeInterval);
-        }),
-      );
-    };
-
-    if (isOpen) {
-      this.setState({ isOpen: false }, () => {
-        this.closeNotification(showNotificationWithStateChanges);
-      });
-    } else {
-      showNotificationWithStateChanges();
+        this.state = {
+            animatedValue: new Animated.Value(0),
+            isOpen: false,
+        };
     }
-  }
 
-  showNotification(done) {
-    Animated.timing(this.state.animatedValue, {
-      toValue: 1,
-      duration: this.props.openCloseDuration,
-      useNativeDriver: true,
-    }).start(done);
-  }
+    show(
+        { title, message, onPress, icon, vibrate, additionalProps } = {
+            title: "",
+            message: "",
+            onPress: null,
+            icon: null,
+            vibrate: true,
+            additionalProps: {},
+        }
+    ) {
+        const { closeInterval } = this.props;
+        const { isOpen } = this.state;
 
-  closeNotification(done) {
-    Animated.timing(this.state.animatedValue, {
-      toValue: 0,
-      duration: this.props.openCloseDuration,
-      useNativeDriver: true,
-    }).start(done);
-  }
+        // Clear any currently showing notification timeouts so the new one doesn't get prematurely
+        // closed
+        clearTimeout(this.currentNotificationInterval);
 
-  render() {
-    const {
-      height: baseHeight,
-      topOffset,
-      backgroundColour,
-      iconApp,
-      notificationBodyComponent: NotificationBody,
-    } = this.props;
+        const showNotificationWithStateChanges = () => {
+            this.setState(
+                {
+                    isOpen: true,
+                    title,
+                    message,
+                    onPress,
+                    icon,
+                    vibrate,
+                    additionalProps,
+                },
+                () =>
+                    this.showNotification(() => {
+                        this.currentNotificationInterval = setTimeout(() => {
+                            this.setState(
+                                {
+                                    isOpen: false,
+                                    title: "",
+                                    message: "",
+                                    onPress: null,
+                                    icon: null,
+                                    vibrate: true,
+                                    additionalProps,
+                                },
+                                this.closeNotification
+                            );
+                        }, closeInterval);
+                    })
+            );
+        };
 
-    const { animatedValue, title, message, onPress, isOpen, icon, vibrate } = this.state;
+        if (isOpen) {
+            this.setState({ isOpen: false }, () => {
+                this.closeNotification(showNotificationWithStateChanges);
+            });
+        } else {
+            showNotificationWithStateChanges();
+        }
+    }
 
-    const height = baseHeight + this.heightOffset;
+    showNotification(done) {
+        Animated.timing(this.state.animatedValue, {
+            toValue: 1,
+            duration: this.props.openCloseDuration,
+            useNativeDriver: true,
+        }).start(done);
+    }
 
-    return (
-      <Animated.View
-        style={[
-          styles.notification,
-          { height, backgroundColor: backgroundColour },
-          {
-            transform: [
-              {
-                translateY: animatedValue.interpolate({
-                  inputRange: [0, 1],
-                  outputRange: [-height + topOffset, 0],
-                }),
-              },
-            ],
-          },
-        ]}
-      >
-        <NotificationBody
-          title={title}
-          message={message}
-          onPress={onPress}
-          isOpen={isOpen}
-          iconApp={iconApp}
-          icon={icon}
-          vibrate={vibrate}
-          onClose={() => this.setState({ isOpen: false }, this.closeNotification)}
-          additionalProps={this.state.additionalProps}
-        />
-      </Animated.View>
-    );
-  }
+    closeNotification(done) {
+        Animated.timing(this.state.animatedValue, {
+            toValue: 0,
+            duration: this.props.openCloseDuration,
+            useNativeDriver: true,
+        }).start(done);
+    }
+
+    render() {
+        const {
+            height: baseHeight,
+            topOffset,
+            backgroundColour,
+            iconApp,
+            notificationBodyComponent: NotificationBody,
+        } = this.props;
+
+        const {
+            animatedValue,
+            title,
+            message,
+            onPress,
+            isOpen,
+            icon,
+            vibrate,
+        } = this.state;
+
+        const height = baseHeight + this.heightOffset;
+
+        return (
+            <Animated.View
+                style={[
+                    styles.notification,
+                    { height, backgroundColor: backgroundColour },
+                    {
+                        transform: [
+                            {
+                                translateY: animatedValue.interpolate({
+                                    inputRange: [0, 1],
+                                    outputRange: [-height + topOffset, 0],
+                                }),
+                            },
+                        ],
+                    },
+                ]}
+            >
+                <NotificationBody
+                    title={title}
+                    message={message}
+                    onPress={onPress}
+                    isOpen={isOpen}
+                    iconApp={iconApp}
+                    icon={icon}
+                    vibrate={vibrate}
+                    onClose={() =>
+                        this.setState({ isOpen: false }, this.closeNotification)
+                    }
+                    additionalProps={this.state.additionalProps}
+                />
+            </Animated.View>
+        );
+    }
 }
 
 Notification.propTypes = {
-  closeInterval: PropTypes.number,
-  openCloseDuration: PropTypes.number,
-  height: PropTypes.number,
-  topOffset: PropTypes.number,
-  backgroundColour: PropTypes.string,
-  notificationBodyComponent: PropTypes.oneOfType([PropTypes.node, PropTypes.func]),
-  iconApp: Image.propTypes.source,
+    closeInterval: PropTypes.number,
+    openCloseDuration: PropTypes.number,
+    height: PropTypes.number,
+    topOffset: PropTypes.number,
+    backgroundColour: PropTypes.string,
+    notificationBodyComponent: PropTypes.oneOfType([
+        PropTypes.node,
+        PropTypes.func,
+    ]),
+    iconApp: any,
 };
 
 Notification.defaultProps = {
-  closeInterval: 4000,
-  openCloseDuration: 200,
-  height: 80,
-  topOffset: 0,
-  backgroundColour: 'white',
-  notificationBodyComponent: DefaultNotificationBody,
-  iconApp: null,
+    closeInterval: 4000,
+    openCloseDuration: 200,
+    height: 80,
+    topOffset: 0,
+    backgroundColour: "white",
+    notificationBodyComponent: DefaultNotificationBody,
+    iconApp: null,
 };
 
 export default Notification;
